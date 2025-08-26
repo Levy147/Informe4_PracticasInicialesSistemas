ce3d92c9f65971727d2dcfb4c301631233bd6388
Levy147 <113801445+Levy147@users.noreply.github.com>
2025-08-26 16:17:15 -0600

    Delete eV1.sql
    Archivo Erroneo

--- eV1.sql
+++
@@ -1,2 +0,0 @@
-[33m68d2b08[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m Agregar script SQL para crear base de datos del sistema educativo
-[33md174bd6[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m first commit
